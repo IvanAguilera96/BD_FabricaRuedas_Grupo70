ab3141dd3d1dbaea778dfb2a9f270087b51adce9
Leonardo <leomicale.lm@gmail.com>
2026-06-10 17:14:42 -0300

    se agrega trigger validarCuitCliente

--- Script_5_Trigger.sql
+++ Script_5_Trigger.sql
@@ -1,4 +1,3 @@
-
 USE TP_FabricaRuedas;
 GO
 
@@ -7,7 +6,7 @@ ON StockRuedas
 AFTER UPDATE
 AS
 BEGIN
-    --Si el stock baja de 10 unidades
+    --Si el stock baja de 10 unidades cruzando la línea límite
     IF EXISTS (
         SELECT 1 
         FROM Inserted i 
@@ -22,14 +21,68 @@ BEGIN
         FROM Inserted i
         WHERE i.CantDisponible < 10;
 
-        PRINT 'El stock de los siguientes modelos ha caido por debajo del limite de 10 unidades:';
-        PRINT SUBSTRING(@ModelosAfectados, 1, LEN(@ModelosAfectados) - 1);
-    END
+        --Protección contra cadenas vacías o errores de SUBSTRING
+        IF LEN(@ModelosAfectados) > 0
+        BEGIN
+            PRINT 'El stock de los siguientes modelos ha caido por debajo del limite de 10 unidades:';
+            PRINT SUBSTRING(@ModelosAfectados, 1, LEN(@ModelosAfectados) - 1);
+        END
+    END 
 END;
 GO
 
---CREATE TRIGGER TR_ValidadCuitCliente 
+CREATE TRIGGER TR_ValidarCuitCliente
+ON Clientes
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    --Valida si el CUIT ingresado cumple con el patron de 13 caracteres: XX-XXXXXXXX-X
+    --[0-9] significa número, el guion es literal
+    IF EXISTS (
+        SELECT 1 
+        FROM Inserted 
+        WHERE Cuit NOT LIKE '[0-9][0-9]-[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-[0-9]'
+    )
+    BEGIN
+        ROLLBACK TRANSACTION;
+        
+        RAISERROR ('Formato de CUIT inválido. Debe respetar la estructura: XX-XXXXXXXX-X.', 16, 1);
+        RETURN;
+    END
+END;
+GO 
+
+CREATE TRIGGER TR_RestaurarStockPorCancelacion
+ON DetalleVentas
+AFTER DELETE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+        BEGIN TRANSACTION;
+
+        -- 1. DEVOLUCIÓN FÍSICA: Sumamos las cantidades al stock de ruedas
+        UPDATE sr
+        SET sr.CantDisponible = sr.CantDisponible + d.Cantidad
+        FROM StockRuedas sr
+        INNER JOIN deleted d ON sr.IdRueda = d.IdRueda;
+
+        -- 2. DEVOLUCIÓN ECONÓMICA: Restamos el dinero de la factura cancelada en la cabecera
+        UPDATE v
+        SET v.MontoTotal = v.MontoTotal - (d.Cantidad * d.PrecioUnitario)
+        FROM Ventas v
+        INNER JOIN deleted d ON v.IdVenta = d.IdVenta;
+
+        COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+
+        RAISERROR ('Error al restaurar el stock y actualizar el monto de la venta.', 16, 1);
+    END CATCH
+END;
+GO
 
 
-
---CREATE TRIGGER TR_RestaurarStockPorCancelacion
