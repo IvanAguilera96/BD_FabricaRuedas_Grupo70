3f75ff459f67c72b83e0dff5bea7f0699faaf6eb
Leonardo <leomicale.lm@gmail.com>
2026-06-10 23:31:55 -0300

    Se agregan clases en la capa dominio.

---
+++ FabricaRuedasApp/Dominio/DetalleVenta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion.Dominio
+{
+    public class DetalleVenta
+    {
+        public int IdDetalle { get; set; }
+        public int IdVenta { get; set; }
+        public int IdRueda { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+
+        //Calcula el subtotal por producto/s que registra la venta.
+        public decimal Subtotal
+        {
+            get
+            {
+                return Cantidad * PrecioUnitario;
+            }
+        }
+    }
+}
---
+++ FabricaRuedasApp/Dominio/Venta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion.Dominio
+{
+    public class Venta
+    {
+        public int IdVenta { get; set; }
+        public int IdCliente { get; set; }
+        public DateTime FechaVenta { get; set; }
+        public decimal MontoTotal { get; set; }
+
+        
+        public List<DetalleVenta> Detalle { get; set; }
+
+        public Venta()
+        {
+            Detalle = new List<DetalleVenta>();
+        }
+    }
+}
